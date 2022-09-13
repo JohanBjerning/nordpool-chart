adc150146a951a361efa23699712e3899ee783bc
Johan Bjerning <johan@bjerning.se>
2022-09-13 16:13:16 +0200

    Fix crash in function2

--- netlify/functions/fetchData.js
+++ netlify/functions/fetchData.js
@@ -4,12 +4,6 @@ exports.handler = async (event, context) => {
   let api_url = process.env.API_URL;
   let api_token = process.env.API_TOKEN;
   
-  console.log(api_url);
-  
-  var myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
-  myHeaders.append("Authorization", "Bearer " + api_token);
-
   const res = await axios.get(api_url, {
     headers: {
       "Authorization":  "Bearer " + api_token
