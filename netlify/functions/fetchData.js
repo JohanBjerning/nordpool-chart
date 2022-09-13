83f450df6ac3696b71d1871a5a7c70a9d8733b96
Johan Bjerning <johan@bjerning.se>
2022-09-13 15:53:47 +0200

    Change for functions

---
+++ netlify/functions/fetchData.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+
+exports.handler = async (event, context) => {
+  let api_url = process.env.API_URL;
+  let api_token = process.env.API_TOKEN;
+  
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  myHeaders.append("Authorization", "Bearer " + api_token);
+
+  const res = await axios.get(api_url, {
+    headers: {
+      "Authorization":  "Bearer " + api_token
+    }
+  }).catch(console.log("error"));  
+
+  const data = res.data;
+  
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data)
+  }
+} 
